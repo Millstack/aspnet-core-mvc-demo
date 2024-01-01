518c4a48a712497dd27684c3c30e0813e1b6b077
Milind Khamkar <fullstackwithmilind@gmail.com>
2024-01-01 16:40:47 +0530

    .net core mvc blog

---
+++ Election PMS/Controllers/AdminTagController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Election_PMS.Controllers
+{
+    public class AdminTagController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
